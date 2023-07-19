f2b7401f26c046b64220761b55a6829db704c8c2
Mustafa <mustafapaydass@gmail.com>
2023-07-19 15:02:38 +0300

    add javax

--- src/main/java/com/library/libraryApi/configuration/SecurityConfig.java
+++ src/main/java/com/library/libraryApi/configuration/SecurityConfig.java
@@ -1,35 +1,46 @@
 package com.library.libraryApi.configuration;
 
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.http.SessionCreationPolicy;
+
 import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-public class SecurityConfig {
+@CrossOrigin
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig implements SecurityFilterChain {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/**").fullyAuthenticated();
-                })
+        return http.authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
+                }).addFilterBefore(new TokenRequestFilter(), BearerTokenAuthenticationFilter.class)
                 .oauth2ResourceServer((oauth2) -> oauth2
-                        .jwt(Customizer.withDefaults())).addFilterBefore(new TokenRequestFilter(),
-                        BearerTokenAuthenticationFilter.class);
-
-        return http.build();
+                        .jwt(Customizer.withDefaults())).sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults()).build();
     }
+
+
 
 //    @Bean
 //    public JwtAuthenticationConverter jwtAuthenticationConverterForKeycloak() {
@@ -67,5 +78,17 @@ public class SecurityConfig {
         return source;
 
 
+    }
+
+    @Override
+    public boolean matches(HttpServletRequest request) {
+        return false;
+    }
+
+    @Override
+    public List<Filter> getFilters() {
+        List<Filter> filters = new ArrayList<>();
+        filters.add(new TokenRequestFilter());
+        return filters;
     }
 }
--- src/main/java/com/library/libraryApi/configuration/TokenRequestFilter.java
+++ src/main/java/com/library/libraryApi/configuration/TokenRequestFilter.java
@@ -4,16 +4,29 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
 
+import java.io.IOException;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+@Component
 public class TokenRequestFilter extends OncePerRequestFilter {
+    private final Logger LOGGER = LoggerFactory.getLogger(TokenRequestFilter.class);
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if(request.getUserPrincipal() != null){
-            System.out.println(request.getUserPrincipal());
+
         }
+        LOGGER.debug("xxxxxxxxxxx");
         filterChain.doFilter(request,response);
+
     }
+
 }
